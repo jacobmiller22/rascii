38b41dde8c978e67e5eb8674bc8c365def848b97
Jacob Miller <dev@jacobmiller22.com>
2023-01-04 18:03:37 -0500

    Major refactor

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,108 @@
-use std;
-mod lib;
-use crate::lib::{read_image, paint_image_as_ascii, print_usage, AspectRatio};
-#[derive(Default)]
-struct Args {
-    file_path: String,
-    samplings: String,
-    aspect_ratio: AspectRatio,
-    palette: String,
-}
+// use std;
 
-fn parse_args() ->Args {
-    let args: Vec<String> = std::env::args().collect();
-    
-    if args.len() < 2 {
-        print_usage(&args[0]);
-        std::process::exit(0);
-    }   
+use clap::Parser;
+use image::{
+    imageops::FilterType, io::Reader as ImageReader, DynamicImage, GenericImageView, Pixel, Pixels,
+    Rgba,
+};
 
-    let mut flag = None;
+// fn read_image(filename: &str) -> DynamicImage {}
+const ASCII_CHARS: &'static str =
+    "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`\'. ";
 
-    let mut arguments = Args::default();   
-
-    for raw_arg in args.iter() {
-        // Check flags first
-        if flag == Some("-S") {
-            flag = None;
-            // This is value of -S flag
-            arguments.samplings = raw_arg.to_string();
-            continue;
-        } 
-        else if flag == Some("-A") {
-            flag = None;
-            // This is value of -A flag
-            let aspect_ratio_vec = raw_arg.split(":").collect::<Vec<&str>>();
-            if aspect_ratio_vec.len() != 2 {
-                println!("Invalid aspect ratio: {}", raw_arg);
-                std::process::exit(1);
-            }
-            
-            arguments.aspect_ratio.width = Some(aspect_ratio_vec[0].parse::<u32>().unwrap());
-            arguments.aspect_ratio.height = Some(aspect_ratio_vec[1].parse::<u32>().unwrap());
-            continue;
-        } 
-        else if flag == Some("-P") {
-            flag = None;
-            // This is value of -P flag
-            arguments.file_path = raw_arg.to_string();
-            continue;
-        }
-
-        // Then check arguments for flags or arguments
-        let arg = raw_arg.to_lowercase();
-        if arg == "-h" || arg == "--help" {
-            print_usage(&args[0]);
-            std::process::exit(0);
-        }
-        else if arg == "-s" || arg == "--sampling" {
-            flag = Some("-S");
-        }
-        else if arg == "-a" || arg == "--aspect-ratio" {
-            flag = Some("-A");
-        }
-        else if arg == "-p" || arg == "--palette" {
-            flag = Some("-P");
-        } 
-        else {
-            // Must be our file path
-            arguments.file_path = raw_arg.to_string();
-        }
-    }
-    arguments
-}
-
+// The ratio between the width and height of a single character
+const CHAR_RATIO: (u32, u32) = (3, 5); // The width of a character is half its height
 
 fn main() {
-    let args = parse_args();
-    
-    let file_path = args.file_path;
-    
-    let img = read_image(&file_path);
-    paint_image_as_ascii(img);
+    let args: CliArgs = CliArgs::parse();
 
+    println!("path: {:?}", args.path);
+
+    let img: DynamicImage = ImageReader::open(args.path).unwrap().decode().unwrap();
+
+    // Resize the image to a width of 100px
+    let img = img.resize(100, 100, FilterType::Nearest);
+
+    paint(img);
+}
+
+#[derive(Parser)]
+pub struct CliArgs {
+    /// Path to file
+    pub path: std::path::PathBuf,
+    // /// Number of times to greet
+    // #[arg(short, long, default_value_t = 1)]
+    // pub count: u8,
+}
+
+fn paint(img: DynamicImage) {
+    let (width, height) = img.dimensions();
+    let pixels: Pixels<DynamicImage> = img.pixels();
+
+    let mut ascii_img = String::new();
+
+    for (x, y, pixel) in pixels {
+        // // Skip rows and columns to conform to the character ratio
+        // if (CHAR_RATIO.0 != 1 && y % CHAR_RATIO.0 == 0)
+        //     || (CHAR_RATIO.1 != 1 && y % CHAR_RATIO.1 == 0)
+        // {
+        //     continue;
+        // }
+
+        let mut v = vec![pixel]; // The pixels that will be averaged
+
+        // // If the next row or column will be skipped,consider it in the average
+        // if (CHAR_RATIO.0 != 1 && y % CHAR_RATIO.0 == CHAR_RATIO.0 - 1)
+        //     || (CHAR_RATIO.1 != 1 && y % CHAR_RATIO.1 == CHAR_RATIO.1 - 1)
+        // {
+        //     // Get the pixels in the next row or column
+        //     if CHAR_RATIO.0 != 1 && y % CHAR_RATIO.0 == CHAR_RATIO.0 - 1 && y != height - 1 {
+        //         v.push(img.get_pixel(x, y + 1));
+        //     }
+        //     if CHAR_RATIO.1 != 1 && y % CHAR_RATIO.1 == CHAR_RATIO.1 - 1 && x != width - 1 {
+        //         v.push(img.get_pixel(x + 1, y));
+        //     }
+        // }
+
+        // Get the character that corresponds to the pixel
+        let c: char = pixels_to_char(v);
+
+        ascii_img.push(c);
+
+        // Check if we are at the end of a row
+        if x == width - 1 {
+            ascii_img.push('\n');
+        }
+    }
+
+    print!("{}", ascii_img);
+}
+
+fn pixels_to_char(pixels: Vec<Rgba<u8>>) -> char {
+    // Average the pixel values
+    let mut r: u32 = 0;
+    let mut g: u32 = 0;
+    let mut b: u32 = 0;
+
+    let n: u32 = pixels.len() as u32;
+
+    for pixel in pixels {
+        let (pr, pg, pb, _) = pixel.channels4();
+        r += pr as u32;
+        g += pg as u32;
+        b += pb as u32;
+    }
+
+    let avg: u32 = (r + g + b) / (3 * n);
+
+    // Find the character that corresponds to the average pixel value
+    let index = (avg as f32 * ((ASCII_CHARS.len() - 1) as f32 / 255.0)) as usize;
+    // println!(
+    //     "{}, {}, {}",
+    //     avg,
+    //     index,
+    //     ASCII_CHARS.chars().nth(index).unwrap()
+    // );
+
+    ASCII_CHARS.chars().nth(index).unwrap()
 }
